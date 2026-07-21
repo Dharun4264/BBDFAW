ac7d354c2907ad54cf6744c6fe9efa8dd2e35b1a
K Dharun <dharun4264@gmail.com>
2026-07-21 15:38:38 +0530

    fix: built clean relative path assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/BBDFAW/',
+  base: './', // Using relative paths completely bypasses subfolder routing issues on GitHub Pages
 })
