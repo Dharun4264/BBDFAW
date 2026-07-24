9d2385fdec7e8714198b1d9b51ce7168efe036ad
adithya2908 <adithyathangaraj04@gmail.com>
2026-07-24 10:33:44 +0530

    fix: update vite config to expose host port for docker container

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: './',
+  plugins: [react()],
+  server: {
+    host: true, // This is the magic line! It tells Docker to expose the app to the browser.
+    strictPort: true,
+    port: 5173,
+  }
 })
